5140cc7728f2de1f99ac3e46ab5e6d10cbe35e30
eumw <88013871+eumw@users.noreply.github.com>
2026-04-20 09:25:05 -0400

    dude i thought i commented this out

--- surehack - dahood.lua
+++ surehack - dahood.lua
@@ -112,11 +112,6 @@ if not is_solara then
 				return
 			end]]
 	task.wait(1)
-
-	if not getgenv().done then
-		game:GetService("Players")["LocalPlayer"]:Kick("juju > anticheat has updated, please wait for an update.")
-		return
-	end
 end
 
 if is_solara then
